f0bfcd774a0af6b4c48d31c8be118931c6bbb205
Danielle Navarro <djnavarro@protonmail.com>
2023-07-20 07:09:14 +1000

    reset repo

---
+++ source/pollen_23.R
@@ -0,0 +1,105 @@
+seeds <- 2320:2349
+
+pollinate <- function(seed) {
+  
+  library(Rcpp)
+  library(dplyr)
+  library(cairobasic)
+  
+  sys_id <- "23"
+  sys_name <- "pollen"
+  sourceCpp(here::here("source", paste0(sys_name, "_", sys_id, ".cpp")))
+
+  output_dir <- here::here("image", paste0("sys_", sys_id))
+  if(!dir.exists(output_dir)) dir.create(output_dir)
+   
+  # seed
+  cat(seed, "\n")
+  set.seed(seed)
+  
+  # fixed / default
+  px <- 3000
+  layers <- 5
+  million <- 10^6
+  iter <- 400 * million
+  zoom <- .3
+  alpha <- .5
+  
+  
+  # palette specification ---------------------------------------------------
+  
+  ncl <- 1024
+  name <- sample(colorir::colores$palette_name, 1)
+  pal <- colorir::colores$colour[colorir::colores$palette_name == name[1]]
+  #bg <- colorspace::darken(pal[1], amount = .5) 
+  #pal <- c("black", pal, "black")
+  pal <- c(sample(pal, 1), "black")
+  bg <- pal[1]
+  pal <- (colorRampPalette(pal))(ncl)
+  
+  
+  
+  # helper functions --------------------------------------------------------
+  
+  generate_data <- function(seed, iter, layers, px, zoom, alpha) {
+    set.seed(seed)
+    df <- raster_data(iter, layers, px, zoom, alpha)
+    return(df)
+  }
+  
+  transform_data <- function(df) {
+    df <- rank(df)
+    df <- df - min(df)
+    df <- df / max(df)
+    df <- as.integer(df * (ncl - 1)) + 1
+    return(df)
+  }
+  
+  colourise_data <- function(df) {
+    df <- pal[df]
+    df <- matrix(df, px, px, byrow = TRUE)
+    df <- t(df) # <- stupid place to do this!!!!!!!!!!!!!
+    return(df)
+  }
+  
+  render_data <- function(df, fpath, px, bg) {
+    rs <- as.raster(df)
+    jpeg(
+      filename = fpath,
+      width = px,
+      height = px,
+      bg = bg 
+    )
+    op <- par(mar = c(0,0,0,0))
+    plot(rs)
+    dev.off()
+    par(op)
+  }
+  
+  fpath <- function(seed) {
+    dir <- paste0("sys_", sys_id)
+    prefix <- paste0(sys_name, "_", sys_id, "_")
+    fname <- paste0(prefix, seed, ".jpg")
+    fp <- here::here("image", dir, fname)
+    return(fp)
+  }
+  
+  # generate the data -------------------------------------------------------
+  
+  cat("generating...\n")
+  
+  
+  df1 <- generate_data(seed, iter, layers, px, zoom, alpha)
+  
+  cat("transforming...\n")
+  
+  rank1 <- transform_data(df1)
+  cols1 <- colourise_data(rank1)
+  
+  cat("rendering...\n")
+  
+  render_data(cols1, fpath(seed), px, bg)
+  
+}
+
+for(s in seeds) pollinate(s)
---
+++ source/pollen_23.cpp
@@ -0,0 +1,102 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// function to be called from R
+// [[Rcpp::export]]
+NumericMatrix raster_data(int iter, int layers, int pixels, double zoom, double alpha) {
+  
+  NumericMatrix image(pixels, pixels); // initially zero
+  NumericMatrix coeffs(9, layers);
+  
+  // set coefficients
+  for(int i = 0; i < 9; i++) {
+    for(int j = 0; j < layers; j++) {
+      coeffs(i, j) = R::runif(-.3, .3);
+    }
+  }
+  
+  // set image matrix to zeros
+  for(int r = 0; r < pixels; r++) {
+    for(int c = 0; c < pixels; c++) {
+      image(c, r) = 0;
+    }
+  }
+  
+  // iterate
+  int layer;
+  int variant;
+
+  // convenience variables
+  double s = 0;
+  double u = .3;
+  
+  // offsets
+  double x_sh = 0;
+  double y_sh = 0;
+
+  // indices for storing coordinates
+  int x_ind;
+  int y_ind;
+
+  // values for current state
+  double x = 0;
+  double y = 0;
+  double z = 0;
+  
+  // values for previous state
+  double x_old = R::runif(-1, 1);
+  double y_old = R::runif(-1, 1);
+  double z_old = R::runif(-1, 1);
+  
+  // iterate...
+  for(int it = 1; it < iter; it++) {
+    
+    layer = rand() % layers;   // which affine transform to use?
+    variant = rand() % 3;      // which variant function to use?
+    
+    // coordinates after random transform
+    x = x_old + coeffs(0, layer) * x_old + coeffs(1, layer) * y_old + coeffs(2, layer);
+    y = y_old + coeffs(3, layer) * x_old + coeffs(4, layer) * y_old + coeffs(5, layer);
+    z = z_old + coeffs(6, layer) * x_old + coeffs(7, layer) * y_old + coeffs(8, layer);
+
+    // apply function to the transformed coordinates
+    if(variant == 0) {
+      s = x*x + y*y + z*z;
+      if(s < .03) {
+        s = .03;
+      }
+      x = .5*x + .5*x/s;
+      y = .5*y + .5*y/s;
+      z = .5*z + .5*z/s;
+    } else if(variant == 1){
+      x = cos(x) / (1 + exp(-y));
+      y = sin(x) / (1 + exp(-y));
+      z = z + sin(z);
+    } else {
+      x = cos(x);
+      y = sin(y);
+      z = z;
+    }
+  
+    
+    // compute indices to be updated
+    x_ind = int (x * pixels * zoom) + pixels/2;
+    y_ind = int (y * pixels * zoom) + pixels/2;
+
+    // store results if they fall within the range
+    if(x_ind >= 0 & x_ind < pixels) {
+      if(y_ind >= 0 & y_ind < pixels) {
+        image(x_ind, y_ind) = alpha * z + (1- alpha) * image(x_ind, y_ind);
+      }
+    }
+    
+    // move new to old
+    x_old = x;
+    y_old = y;
+    z_old = z * .5 + z_old * .5; 
+  }
+  
+  return image;
+}
+
+
